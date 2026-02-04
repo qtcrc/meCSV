59d2f692b0554c430ca5fe41b32652e4a7df9ede
loop-nop <86272232+loop-nop@users.noreply.github.com>
2026-02-04 15:21:14 +0000

    Validate file paths

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from itertools import count
+import os
 import argparse
 import csv
 import tabulate as TB
+import logging
 
 
 class RawRow:
@@ -19,23 +22,44 @@ class RawRow:
         self.src = str(src)
 
 
-def reader(files):
+def genIDs():
+    return map(lambda x: x+1, count(0))
+
+
+def validate_files(files):
+    OK = True
+    for filepath in files:
+        if not os.path.exists(filepath):
+            logging.error(f"Not found: {filepath}")
+            OK = False
+    return OK
+
+
+def make_reader(files):
+    if not validate_files(files):
+        return None
+
+    return iter(_reader(files))
+
+
+def _reader(files):
+
     for csv_file_path in files:
         with open(str(csv_file_path)) as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
-            colums = reader.__next__()
+            columns = next(reader)  # skip columns
+            # print("columns:", columns)
 
-            print("columns ->", colums)
             for row in reader:
                 yield RawRow(*tuple(row), src=csv_file_path)
 
 
-def report_avg_gdp(files):
+def report_avg_gdp(reader):
     db = {}  # {"country" : {2020 : [gdp, gdp]}}
     results = []
 
     # build db
-    for row in reader(files):
+    for row in reader:
         db.setdefault(row.country, {}).setdefault(
             row.year, []).append(row.gdp)
 
@@ -49,6 +73,7 @@ def report_avg_gdp(files):
 
     # sort
     results.sort(key=lambda g: -g[1])
+    results.insert(0, ["country", "gdp"])
 
     return results
 
@@ -57,16 +82,33 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-f", "--files", dest="files",
-                        nargs="*", help="csv files to parce")
+                        nargs="*", help="csv files to parce", required=True)
     parser.add_argument("-r", "--report", dest="report_type",
-                        nargs=1, help="average-gdp")
+                        nargs=1, help="reprots methods", required=True,
+                        choices=["average-gdp"])
+    # TODO filters
+    # parser.add_argument("-y", "--year-range", dest="list_year",
+    #                     nargs=1, required=False,
+    #                     help="example: '2000' or '2000-2025'")
+    # parser.add_argument("-c", "--countrys", dist="list_country",
+    #                     nargs=1, required=False,
+    #                     help="example: '!Russia, Belarus, Greece'")
+    # parser.add_argument("-C", "--continents", dist="list_continents",
+    #                     nargs=1, required=False,
+    #                     help="example: 'Europe, Africa, Asia'")
+
     args = parser.parse_args()
+    reader = make_reader(args.files)
+    if not reader:
+        quit(1)
 
-    print(args)
-
-    table = report_avg_gdp(args.files)
-    headers = ["Country", "avg GDP(desc)"]
-    print(TB.tabulate(table,
-                      headers=headers, tablefmt="github",
-                      floatfmt=".2f", showindex=True)
-          )
+    table = []
+    match args.report_type[0]:
+        case "average-gdp":
+            table = report_avg_gdp(reader)
+            print(TB.tabulate(table,
+                              headers="firstrow", tablefmt="psql",
+                              floatfmt=".2f", showindex=genIDs())
+                  )
+        case _:
+            assert False
